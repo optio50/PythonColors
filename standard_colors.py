4ad99e66edb6ba44c302d5e2a89538b08ea6386f
Your Name <you@example.com>
2026-04-09 22:43:33 -0800

    Fix bugs, typos, and improve code quality
    color-test.py:
    - Fix default fg/bg escape codes (256 is out of range; use 39/49)
    - Fix misty_rose1 typo -> misty_rose_1 in fg and bg
    - Replace hardcoded color_names list with vars(colors.fg) derivation
    - Replace while loop with enumerate; move fg/bg instances outside loop
    
    standard_colors.py:
    - Fix garbled docstring
    - Fix black index 16 -> 0 (true black) in fg and bg
    - Add missing bg.pink and bg.light_pink
    - Add missing colors.reset to light_red, light_gray, light_blue, green prints
    - Replace commented-out stand_colors() call with __main__ guard

--- standard_colors.py
+++ standard_colors.py
@@ -6,9 +6,8 @@ fg for foreground
 bg for background
 use as  colors.subclass.colorname
 i.e. colors.fg.red or colors.bg.green
-also, colors.reverse, the generic bold, colors.reverse, disable
-colors.reverse, underline, colors.reverse, reverse, colors.reverse, strike through, 
-colors.reverse, and invisible work with the main class i.e. colors.bold """
+also, the generic bold, disable, underline, reverse, strikethrough,
+and invisible work with the main class i.e. colors.bold """
 
 
 class colors:
@@ -29,7 +28,7 @@ class colors:
         gray = '\033[38;5;240m'
         light_gray = '\033[38;5;246m'
         white = '\033[38;5;15m'
-        black = '\033[38;5;16m'
+        black = '\033[38;5;0m'
         orange = '\033[38;5;202m'
         light_orange = '\033[38;5;172m'
         blue = '\033[38;5;21m'
@@ -51,7 +50,7 @@ class colors:
         gray = '\033[48;5;240m'
         light_gray = '\033[48;5;246m'
         white = '\033[48;5;15m'
-        black = '\033[48;5;16m'
+        black = '\033[48;5;0m'
         orange = '\033[48;5;202m'
         light_orange = '\033[48;5;172m'
         blue = '\033[48;5;21m'
@@ -62,16 +61,18 @@ class colors:
         light_purple = '\033[48;5;99m'
         yellow = '\033[48;5;220m'
         light_yellow = '\033[48;5;227m'
+        pink = '\033[48;5;201m'
+        light_pink = '\033[48;5;206m'
 
 
 # Demo
 def stand_colors():
     print(colors.blink, colors.fg.red, colors.reverse, f"{'Blinking & Red'.center(30)}", colors.reset, sep="")
-    print(colors.fg.light_red, colors.reverse, f"{'Light Red'.center(30)}", sep="")
+    print(colors.fg.light_red, colors.reverse, f"{'Light Red'.center(30)}", colors.reset, sep="")
     print(colors.fg.cyan, colors.reverse, f"{'Cyan'.center(30)}", colors.reset, sep="")
     print(colors.bold, colors.fg.light_cyan, colors.reverse, f"{'Bold & Light Cyan'.center(30)}", colors.reset, sep="")
     print(colors.fg.gray, colors.reverse, f"{'Gray'.center(30)}", colors.reset, sep="")
-    print(colors.fg.light_gray, colors.reverse, f"{'Light Gray'.center(30)}", sep="")
+    print(colors.fg.light_gray, colors.reverse, f"{'Light Gray'.center(30)}", colors.reset, sep="")
     print(colors.fg.white, colors.reverse, f"{'White'.center(30)}", colors.reset, sep="")
     print(colors.fg.orange, colors.reverse, f"{'Orange'.center(30)}", colors.reset, sep="")
     print(colors.fg.light_orange, colors.reverse, f"{'Light Orange'.center(30)}", colors.reset, sep="")
@@ -79,8 +80,8 @@ def stand_colors():
     print(colors.fg.light_pink, colors.reverse, f"{'Light Pink'.center(30)}", colors.reset, sep="")
     print(colors.fg.white, colors.bg.black, f"{'Black'.center(30)}", colors.reset, sep="")
     print(colors.fg.blue, colors.reverse, f"{'Blue'.center(30)}", colors.reset, sep="")
-    print(colors.fg.light_blue, colors.reverse, f"{'Light Blue'.center(30)}", sep="")
-    print(colors.fg.green, colors.reverse, f"{'Green'.center(30)}", sep="")
+    print(colors.fg.light_blue, colors.reverse, f"{'Light Blue'.center(30)}", colors.reset, sep="")
+    print(colors.fg.green, colors.reverse, f"{'Green'.center(30)}", colors.reset, sep="")
     print(colors.underline, colors.fg.light_green, colors.reverse, f"{'Underline & Light Green'.center(30)}", colors.reset, sep="")
     print(colors.fg.purple, colors.reverse, f"{'Purple'.center(30)}", colors.reset, sep="")
     print(colors.fg.light_purple, colors.reverse, f"{'Light Purple'.center(30)}", colors.reset, sep="")
@@ -89,3 +90,6 @@ def stand_colors():
 
 # Color Demonstration. Uncomment below to run demo
 #stand_colors()
+
+if __name__ == '__main__':
+    stand_colors()
